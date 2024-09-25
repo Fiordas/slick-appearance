22793a4bfa708b6750e4a554f614562282f7c8a5
Simonas Juska <simonas.juska@outlook.com>
2024-09-25 23:05:06 +0300

    chore(web): update build process

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const { name } = require('./package.json');
+const Q3Rcon = require('quake3-rcon');
+require('dotenv').config({ path: '../.env' });
+
+const rcon = new Q3Rcon({
+  address: process.env.ADDRESS,
+  port: process.env.PORT,
+  password: process.env.PASSWORD,
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+  plugins: [
+    react(),
+    {
+      name: 'run-script-on-change',
+      apply: 'serve',
+      handleHotUpdate({ file }) {
+        console.log(`${file} has changed. Restarting resource...`);
+        rcon.send(`restart ${name}`, () => {
+          console.log(`Resource ${name} restarted`);
+        });
+      },
+    },
+  ],
+  base: './',
 });
